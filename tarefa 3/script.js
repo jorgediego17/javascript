188d61afbaac7e819805a685ba4086697379b83f
jorgediego17 <56599435+jorgediego17@users.noreply.github.com>
2020-01-16 09:25:23 +0100

    Add files via upload

---
+++ tarefa 3/script.js
@@ -0,0 +1,24 @@
+/*
+
+Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: nada.
+*/
+
+/*
+sin comillas hace operaciones matematicas, con comillas no realiza ninguna operacion con lo que se introduce
+*/
+var buenas = Math.PI;
+var xd = 9.2554;
+var jsd = 25.2222;
+var pedro = 16.7656;
+var omg = 2.2134;
+var raiz1 = Math.sqrt(xd);
+var raiz2 = Math.sqrt(jsd);
+var raiz3 = Math.sqrt(pedro);
+var mates = Math.pow(omg,2);
+alert(raiz1);
+alert(raiz2);
+alert(raiz3);
+alert(omg);
+alert(buenas)

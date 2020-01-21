91c0894d679d90874de550ec2007a47ac8379898
jorgediego17 <56599435+jorgediego17@users.noreply.github.com>
2020-01-21 09:27:32 +0100

    Add files via upload

---
+++ tarefa 8/script.js
@@ -0,0 +1,27 @@
+﻿/*
+
+Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: nada.
+*/
+
+/*
+sin comillas hace operaciones matematicas, con comillas no realiza ninguna operacion con lo que se introduce
+*/
+
+
+function TipoMedalla(posicion){
+	if(posicion == 1)
+	{medalla = "Medalla de ouro";}
+	else if (posicion == 2)
+	{medalla = "Medalla de prata";}
+	else if (posicion == 3)
+	{medalla = "Medalla de bronce";}
+	else { medalla = "Mala sorte, outra vez será";}
+}
+var posicion = prompt("En que posto quedaches?");
+posicion = parseFloat(posicion);
+
+var funcionaza = TipoMedalla(posicion);
+document.write(posicion + "º");
+document.write(medalla);

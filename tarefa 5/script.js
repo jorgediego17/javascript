c78c0111beff4f625fa5b9f648b18b158b9029e3
jorgediego17 <56599435+jorgediego17@users.noreply.github.com>
2020-01-21 09:25:12 +0100

    Add files via upload

---
+++ tarefa 5/script.js
@@ -0,0 +1,26 @@
+﻿/*
+
+Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: nada.
+*/
+
+/*
+sin comillas hace operaciones matematicas, con comillas no realiza ninguna operacion con lo que se introduce
+*/
+
+var ducias;
+
+var numducias;
+
+ducias = prompt("Cantas ducias de ovos leva unha tortilla?");
+
+numducias= parseInt(ducias);
+
+alert(ducias);
+
+var ovos;
+
+ovos = 12* numducias;
+
+alert(ovos);

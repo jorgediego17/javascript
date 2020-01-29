9376b3ad094fefd1a93122068b05331da1b36bc9
jorgediego17 <56599435+jorgediego17@users.noreply.github.com>
2020-01-29 10:34:43 +0100

    Add files via upload

---
+++ Proxecto1caixaveloz/script.js
@@ -0,0 +1,26 @@
+﻿/*
+
+Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: nada.
+*/
+
+/*
+sin comillas hace operaciones matematicas, con comillas no realiza ninguna operacion con lo que se introduce
+*/
+
+
+var producto1;
+producto1 = prompt("Precio primer producto?");
+var precio1;
+precio1 = parseFloat(producto1);
+var producto2;
+producto2 = prompt("Precio segundo producto?");
+var precio2;
+precio2 = parseFloat(producto2);
+var producto3;
+producto3 = prompt("Precio tercer producto?");
+var precio3;
+precio3 = parseFloat(producto3);
+var total = precio1+precio2+precio3;
+document.write(total);
